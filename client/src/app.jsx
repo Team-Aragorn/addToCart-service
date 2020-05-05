091d366d227a23f185fd0d227f8ed029723897ed
Michael <griffinvmichael@gmail.com>
2020-05-05 13:48:23 -0700

    express now handles game id urls, frontend still WIP

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -15,15 +15,43 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //
+      gameid: 0,
+      game: [],
+      loading: true,
     }
+    this.getGame = this.getGame.bind(this);
+    this.url = 'http://localhost:3001/game/'
   }
 
   componentDidMount() {
-    //
+    const gid = window.location.pathname.match(/\/game\/(\d+)\//);
+    console.log("GID", gid)
+    this.setState({
+      gameid: gid[1],
+    }, () => {
+      this.getGame();
+    })
+  }
+
+  getGame() {
+    const { gameid } = this.state;
+
+    fetch(`${this.url}${gameid}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          game: [data],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   render() {
+    if(this.state.loading) {
+      return null;
+    }
     return (
       <>
         <TitleAndRating />
--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ const PORT = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use('/game/:id', express.static(__dirname + '/../client/dist'));
 
 //TO-DO: api get router
-app.get('/api/game/:id', (req, res) => {
+app.get('/game/:id', (req, res) => {
   const id = Number(path.basename(req.url));
   db.getGame(id, (err, result) => {
     if(err) {
-      res.sendStatus(500);
+      res.sendStatus(500).send(err);
       res.end();
     } else {
       res.send(result).json();
       res.end();
     }
-  })
-})
+  });
+});
 
 
 app.listen(PORT, () => {
