900a8ae4ef8dba5710560481ab7132c762b21a52
Michael <griffinvmichael@gmail.com>
2020-05-06 09:36:36 -0700

    in/out of stock text color dependent on state now

--- client/src/components/InStockAndShippingTime.jsx
+++ client/src/components/InStockAndShippingTime.jsx
@@ -11,24 +11,17 @@ class InStockAndShippingTime extends React.Component {
   componentDidMount() {
     if(this.props.game[0].inStock) {
       console.log("HELLO YES IN STOCK HAPPY!");
+
       this.setState({
         inStock: true,
       })
     };
   };
 
-  // function inStock(props) {
-  //   return <div className="in-stock">IN STOCK</div>
-  // }
-
-  // function outStock(props) {
-  //   return <div className="out-stock">OUT OF STOCK</div>
-  // }
-
   render() {
     return (
       <>
-        <div className="inStock">
+        <div className="inStock" style={ { color: this.state.inStock ? 'green' : 'red' } }>
           {this.state.inStock ? 'IN STOCK' : 'OUT OF STOCK'}
         </div>
         <div className="shippingTime">
