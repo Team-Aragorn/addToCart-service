f84ffd211991818af0f3b525a31a2f4f70f25494
Michael <griffinvmichael@gmail.com>
2020-05-05 19:17:39 -0700

    renders each items 'in/out of stock' correctly

--- client/src/components/InStockAndShippingTime.jsx
+++ client/src/components/InStockAndShippingTime.jsx
@@ -4,15 +4,32 @@ class InStockAndShippingTime extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //
+      inStock: false,
     }
   }
+
+  componentDidMount() {
+    if(this.props.game[0].inStock) {
+      console.log("HELLO YES IN STOCK HAPPY!");
+      this.setState({
+        inStock: true,
+      })
+    };
+  };
+
+  // function inStock(props) {
+  //   return <div className="in-stock">IN STOCK</div>
+  // }
+
+  // function outStock(props) {
+  //   return <div className="out-stock">OUT OF STOCK</div>
+  // }
 
   render() {
     return (
       <>
         <div className="inStock">
-          {this.props.game[0].inStock}
+          {this.state.inStock ? 'IN STOCK' : 'OUT OF STOCK'}
         </div>
         <div className="shippingTime">
          Usually ships within 24hrs. (placeholder)
