5f287f447224bace09467c1e0f11a14bade4d73c
Michael <griffinvmichael@gmail.com>
2020-05-10 11:52:25 -0700

    formatted GamePrices HTML to be readable

--- client/src/components/GamePrices.jsx
+++ client/src/components/GamePrices.jsx
@@ -34,11 +34,38 @@ class GamePrices extends React.Component {
     return (
       <div className="gamePrices">
 
-        <div className="newPrice" onClick={this.divClick}><div className="mapMarker"><FontAwesomeIcon icon="map-marker-alt" /></div><div className="new">NEW</div><div className="nPrice">${this.props.game[0].newPrice}.00</div></div>
+        <div className="newPrice" onClick={this.divClick}>
+          <div className="mapMarker">
+            <FontAwesomeIcon icon="map-marker-alt" />
+          </div>
+          <div className="new">
+            NEW
+          </div>
+          <div className="nPrice">
+            ${this.props.game[0].newPrice}.00
+          </div>
+        </div>
 
-        <div className="usedPrice" onClick={this.divClick}><div className="mapMarker"><FontAwesomeIcon icon="map-marker-alt" /></div><div className="used">PRE-OWNED</div><div className="uPrice">${this.props.game[0].usedPrice}.00</div></div>
+        <div className="usedPrice" onClick={this.divClick}>
+          <div className="mapMarker">
+            <FontAwesomeIcon icon="map-marker-alt" />
+          </div>
+          <div className="used">
+            PRE-OWNED
+          </div>
+          <div className="uPrice">
+            ${this.props.game[0].usedPrice}.00
+          </div>
+        </div>
 
-        <div className="digitalPrice" onClick={this.divClick}><div className="digital">DIGITAL</div><div className="dPrice">${this.props.game[0].digitalPrice}.00</div></div>
+        <div className="digitalPrice" onClick={this.divClick}>
+          <div className="digital">
+            DIGITAL
+          </div>
+          <div className="dPrice">
+            ${this.props.game[0].digitalPrice}.00
+          </div>
+        </div>
 
       </div>
     )
